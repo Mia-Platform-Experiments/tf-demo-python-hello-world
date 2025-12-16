0b95bfa693cdf0b1a31b373cc253aa40e76be045
Davide Parini <davide.parini@mia-platform.eu>
2025-12-16 13:12:26 +0100

    Try fix python app.

--- app.py
+++ app.py
@@ -17,5 +17,4 @@ def health():
     return jsonify({'status': 'healthy'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
